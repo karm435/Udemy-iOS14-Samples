87caf5cc6db511a3b81636a5be9b562f6c99b8dc
Karm <karmjit435@gmail.com>
2020-07-27 15:29:28 +1000

    Initial Commit of Flash Chat

---
+++ Flash-Chat-iOS14/Flash-Chat-iOS14/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Flash-Chat-iOS14
+//
+//  Created by Karmjit Singh on 27/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Flash Chat").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flash-Chat-iOS14/Flash-Chat-iOS14/Flash_Chat_iOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_Chat_iOS14App.swift
+//  Flash-Chat-iOS14
+//
+//  Created by Karmjit Singh on 27/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Flash_Chat_iOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
